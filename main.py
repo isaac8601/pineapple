43fd055ad0a7580190a9227dc2d1c11fd65060fc
Isaac Jainarine <>
2021-12-09 18:55:50 +0000

    did stuff

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+print('You wake up as God,never brush your teeth, kill your cat becuase cats<dogs')
 
+choice1 = input('Do you do [hw] or play on [relax]/do something fun')
+if choice1== 'relax' or choice1==' relax':
+  print("you end up falling asleep and wake up at 5 and remember hw")
+
+  choice1b=input("you do 'hw' or you 'dont' and keep putting it off")
+  if choice1b=="hw" or choice1b==" hw":
+    print("you get good grades and you keep relaxing the end.")
+  else:
+      print("you do horribly in school and dont really care that much The End.")
+
+
+if choice1 == 'hw' or choice1==' hw':
+  print("You get you homework done fast and get to do something fun, you aso get good grades.")
+
+
+  choice1a=input("you go to the [park] or watch [tv]")
+  if choice1a=="park" or choice1a==" park":
+    print("You see a dog it bites you the end.")
+  else:
+    print("you watch tv and get bored so you read. The End.")
+ 
