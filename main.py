d63c9e89606bd832f00b5afdb56cacc668ee49e0
Gjon Bicaj <95306441+WaterEater20@users.noreply.github.com>
2021-12-14 17:42:02 +0000

    added function

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-print('You wake up as God,never brush your teeth, kill your dog for existing')
+def god():
 
-choice1 = input('[kill] family or [cure] cancer')
-if choice1== 'kill':
-  print("Depressed because you killed your family")
+  print('You wake up as God,never brush your teeth, kill your dog for existing')
 
-  choice1b=input("you love 'life' or you 'dont' and still be depressed")
-  if choice1b=="life" or choice1b==" life":
-    print("live worshipped and adored")
-  else:
-        choice1c=input("Your life ends in eternal suffering because of your lack of self help")
+  choice1 = input('[kill] family or [cure] cancer')
+  if choice1== 'kill':
+    print("Depressed because you killed your family")
 
-elif choice1 == 'cure':
-  print("everybody worships and loves you")
+    choice1b=input("you love 'life' or you 'dont' and still be depressed")
+    if choice1b=="life" or choice1b==" life":
+      print("live worshipped and adored")
+      god()
+    else:
+      print("Your life ends in eternal suffering because of your lack of self help")
+      god()
+
+  elif choice1 == 'cure':
+    print("everybody worships and loves you")
 
 
-  choice1a=input("you go to the [hospital] or watch [monkey's doing math]")
-  if choice1a=="hospital" or choice1a==" hospital":
-    print("cancer patient bites you")
-  else:
-    print("Fall asleep watching monkeys doing math")
- 
+    choice1a=input("you go to the [hospital] or watch [monkey's doing math]")
+    if choice1a=="hospital" or choice1a==" hospital":
+      print("cancer patient bites you")
+      god()
+    else:
+      print("Fall asleep watching monkeys doing math")
+      god()
+god()
